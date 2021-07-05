ee05446c938ef2a1d96e5ff2ffd9aa41109dda7b
izaak svec <slavi.svec@gmail.com>
2021-07-05 16:56:36 +1000

    Fetch coin data API

--- react-crypto-tracker/src/App.jsx
+++ react-crypto-tracker/src/App.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
 import React from 'react'
+import axios from 'axios'
 import './App.scss'
 
-function App() {
+const App = () => {
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data)
+        console.log(res.data)
+      })
+
+      .catch(error => console.error(error))
+  }, [])
+
   return (
     <section className="app">
       <div className="app__header">
