548fd71dbe2e8ce2a72142a37e8d48cf115849fd
izaak svec <slavi.svec@gmail.com>
2021-07-06 17:54:34 +1000

    Added coin details

--- react-crypto-tracker/src/Components/CoinInfo/CoinInfo.jsx
+++ react-crypto-tracker/src/Components/CoinInfo/CoinInfo.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import './CoinInfo.scss'
 
 const CoinInfo = () => {
   return (
-    <div>
-      <img
-        src="https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1605778731"
-        alt="xrp"
-      ></img>
-    </div>
+    <section className="coin">
+      <div className="coin__crypto-coin">
+        <img
+          src="https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1605778731"
+          alt="xrp"
+          className="coin__coin-logo"
+        ></img>
+        <div className="coin__wrapper">
+          <h1 className="coin__name">Ripple</h1>
+          <p className="coin__symbol">xrp</p>
+        </div>
+        <p className="coin__price">0.80</p>
+        <p className="coin__ath">4.00</p>
+        <p className="coin__market-cap">31,235,294,790.36</p>
+      </div>
+    </section>
   )
 }
 
